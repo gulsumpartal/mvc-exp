95068114aadb78baa1b123c9aa9b48f9d062e8a0
gulsumpartal <ummugulsumpartal@gmail.com>
2017-07-31 11:30:51 +0300

    DatabaseContext objesi eklendi.

---
+++ MvcExample/MvcExample.Core/DatabaseContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+using System.Configuration;
+using MvcExample.Domain.User;
+
+namespace MvcExample.Core
+{
+    public class DatabaseContext :DbContext
+    {
+        public DatabaseContext() :base(ConfigurationManager.ConnectionStrings["DefaultConnection"].Name)
+        {
+            
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
