c6c1d62ba82183387690587f5e680e17a8b8c37b
gulsumpartal <ummugulsumpartal@gmail.com>
2017-07-31 15:54:22 +0300

    Role tablosu database e aktarıldı.

--- MvcExample/MvcExample.Core/DatabaseContext.cs
+++ MvcExample/MvcExample.Core/DatabaseContext.cs
@@ -1,6 +1,7 @@
 ﻿using System.Data.Entity;
 using System.Configuration;
 using MvcExample.Domain.Department;
+using MvcExample.Domain.Role;
 using MvcExample.Domain.User;
 
 namespace MvcExample.Core
@@ -19,6 +20,7 @@ namespace MvcExample.Core
 
         public DbSet<User> Users { get; set; }
         public DbSet<Department> Departments { get; set; }
+        public DbSet<Role> Roles { get; set; }
 
         #endregion
 
