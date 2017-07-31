02a8dd466c8b1f6a4c4e26ea761b193abd1c529d
gulsumpartal <ummugulsumpartal@gmail.com>
2017-07-31 11:22:03 +0300

    User class ve entityBase cllass ı eklendi.

---
+++ MvcExample/MvcExample.Domain/Abstract/EntityBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcExample.Domain.Abstract
+{
+    public class EntityBase
+    {
+        [Key]
+        public int Id { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
